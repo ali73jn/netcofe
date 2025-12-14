22a26e89226ea25629ac1ca6c07c85f175af0f20
ali jn <66694170+ali73jn@users.noreply.github.com>
2025-12-14 20:16:36 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
 
+const CACHE_NAME = 'netcofe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './dashboard.css',
+  './dashboard.js',
+  './icons/default_icon.png',
+  './icons/folder.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
